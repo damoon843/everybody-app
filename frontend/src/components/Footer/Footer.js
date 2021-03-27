0d14f225c434b16a8f7299531b3c430c82a8e0b8
Lauren Choi <laurenchoi@gmail.com>
2021-03-27 09:57:28 -0400

    set up frontend structure

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
-import './ProfilePage.css';
+import './Header.css';
 
-export default class ProfilePage extends React.Component {
+export default class Header extends React.Component {
 
   constructor(props) {
     super(props);
@@ -11,8 +11,9 @@ export default class ProfilePage extends React.Component {
 
   render() {
     return (
-      <div className="profile">
-      </div>
+      <footer>
+        
+      </footer>
     );
   }
 }
