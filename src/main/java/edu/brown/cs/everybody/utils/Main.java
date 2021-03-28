7ba77cee7f725d075919c2f7f835f600590581d6
damoon843 <davidmoon@brown.edu>
2021-03-28 16:20:23 -0400

    Containerizing frontend, backend, and DB

---
+++ src/main/java/edu/brown/cs/everybody/utils/Main.java
@@ -0,0 +1,27 @@
+package edu.brown.cs.everybody.utils;
+
+/**
+ * The Main class of our project. This is where execution begins.
+ */
+public final class Main {
+
+  private static final int DEFAULT_PORT = 4567;
+  private final String[] args;
+
+  private Main(String[] args) {
+    this.args = args;
+  }
+
+  /**
+   * The initial method called when execution begins.
+   *
+   * @param args An array of command line arguments
+   */
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+
+  private void run() {
+    System.out.println("HELLO WORLD");
+  }
+}
