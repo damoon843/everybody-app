a1620b5e0a08682cc72485ed9ad4eb42486ce689
Lauren Choi <laurenchoi@gmail.com>
2021-03-30 14:44:12 -0400

    working on home page

--- frontend/src/pages/HomePage/components/Main/Main.js
+++ frontend/src/pages/HomePage/components/Main/Main.js
@@ -1,9 +1,31 @@
-import React from 'react'; 
+import React, {useState, useRef, useEffect} from 'react';
+import GridItem from '../GridItem/GridItem';
 import './Main.css';
 
 function Main() {
+  let workouts = [{title: "Workout 1", duration: "15 min", user: "Tim Nelson"}, {title: "Workout 2", duration: "30 min", user: "Spike"}, 
+  {title: "Workout 3", duration: "20 min", user: "Andy Van Dam"}];
+
+  useEffect(() => {
+    getWorkouts();
+  });
+
+  // TODO: fill this out with a GET request
+  const getWorkouts = () => {
+  }
+
+  const renderWorkouts = () => {
+    const anchor = document.getElementById('grid-anchor')
+    if (workouts) {
+      workouts.forEach(workout => {
+        <GridItem workout={this.workout} />
+      });
+    }
+  }
+
   return (
     <div className="home-main">
+      <div className="grid-anchor"></div>
     </div>
   );
 }
--- frontend/src/pages/HomePage/components/UserProfile/UserProfile.js
+++ frontend/src/pages/HomePage/components/UserProfile/UserProfile.js
@@ -1,9 +1,37 @@
-import React from 'react'; 
+import React, {useState, useRef, useEffect} from 'react';
 import './UserProfile.css';
 
 function UserProfile() {
+  let userInfo = {name: "Jane Doe", followerCount: 123, followingCount: 456, workoutCount: 2};
+
+  useEffect(() => {
+    getUserInfo();
+  }); 
+
+  // TODO: fill this out with a GET request
+  const getUserInfo = () => {
+  }
+
+  console.log(userInfo);
+
   return (
     <div className="user-profile">
+      <img src="https://st3.depositphotos.com/4111759/13425/v/600/depositphotos_134255532-stock-illustration-profile-placeholder-male-default-profile.jpg" alt="profile picture" id="profile-pic" />
+      <h3 id="name">{userInfo.name}</h3>
+      <div className="follow-container">
+        <div className="follow-info">
+          <p id="followers">Followers</p>
+          <h5 id="follower-ct">{userInfo.followerCount}</h5>
+        </div>
+        <div className="follow-info">
+          <p id="following">Following</p>
+          <h5 id="following-ct">{userInfo.followingCount}</h5>
+        </div>
+        <div className="follow-info">
+          <p id="workouts">Workouts</p>
+          <h5 id="workout-ct">{userInfo.workoutCount}</h5>
+        </div>
+      </div>
     </div>
   );
 }
