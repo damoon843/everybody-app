9f1582ce3039981e1089a23378b520eb1e1226c1
Lauren Choi <laurenchoi@gmail.com>
2021-04-04 13:15:01 -0400

    add submit modal to display

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,10 +1,12 @@
 import React from 'react'; 
 import './ProfilePage.css';
+import SubmitModal from '../../components/SubmitModal/SubmitModal'
 
 function ProfilePage(props){
   return (
     <div className="profile-page">
       <h1>This is the profile page</h1>
+      <SubmitModal />
     </div>
   );
 }
