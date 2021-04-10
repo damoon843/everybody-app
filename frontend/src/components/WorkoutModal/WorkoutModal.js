4f7d9751c4f2c990964b6e27d3427578d0ca2faf
Lauren Choi <laurenchoi@gmail.com>
2021-04-10 08:49:30 -0400

    add searchable select option for exercises in workout modal

--- frontend/src/components/WorkoutModal/WorkoutModal.js
+++ frontend/src/components/WorkoutModal/WorkoutModal.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import {Modal, Tabs, Tab, Form, Col, Row, ToggleButtonGroup, ToggleButton, ButtonGroup, Button} from 'react-bootstrap';
 import './WorkoutModal.css';
 import WorkoutItem from './components/WorkoutItem/WorkoutItem'
+import Select from 'react-select'
 
-const sampleData = [{title: "exercise 1", duration: "00:15:00"}, {title: "exercise 2", duration: "00:20:00"}, {title: "exercise 3", duration: "00:10:00"}]
+const sampleData = [{label: "exercise 1", duration: "00:15:00", value: 1}, {label: "exercise 2", duration: "00:20:00", value: 2}, {label: "exercise 3", duration: "00:10:00", value: 3}]
 
 function WorkoutModal(){
   const [show, setShow] = useState(false);
@@ -29,25 +30,29 @@ function WorkoutModal(){
 
       <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>Upload Workout</Modal.Title>
+          <Modal.Title>Create Workout</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form className="exercise-form">
             <Form.Group controlId="exerciseTitle">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Enter exercise title" />
+              <Form.Control type="text" placeholder="Enter workout title" />
             </Form.Group>
 
             <Form.Group controlId="exerciseDesc">
               <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" rows={3} type="text" placeholder="Enter a description of your exercise" />
+              <Form.Control as="textarea" rows={3} type="text" placeholder="Enter a description of your workout" />
             </Form.Group>
 
             <Form.Group controlId="exercises">
               <Form.Label>Select Exercises</Form.Label>
-              <div>
-                {allExercises}
-              </div>
+              <Select
+                isMulti
+                name="colors"
+                options={sampleData}
+                className="basic-multi-select"
+                classNamePrefix="select"
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
