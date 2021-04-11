dd99226ffa6771ac98a32c65e59bb221d8f4fe94
aguo71 <yingzhe_guo@brown.edu>
2021-04-11 04:30:08 -0400

    updated AppUser/Workout classes

--- src/main/java/edu/brown/cs/everybody/data/PostgresDatabase.java
+++ src/main/java/edu/brown/cs/everybody/data/PostgresDatabase.java
@@ -18,27 +18,31 @@ import java.util.PriorityQueue;
  * its related SQL commands.
  */
 public final class PostgresDatabase {
-  private static PostgresDatabase singleInstance = null;
+//  private static PostgresDatabase singleInstance = null;
   private static Connection dbConn = null;
 
   // TODO: cache getUser?
+  // main.java concurrency (stretch feature)
+  // cache of users, getUser/getFollowers query called in constructor so also cached?
+
+
   /* Hidden constructor to avoid instantiation */
   private PostgresDatabase() {
   }
 
-  /**
-   * Singleton getter for PostgresDatabase and sets up DB connection.
-   * @return single instance of PostgresDatabase.
-   */
-  public static PostgresDatabase getInstance() throws URISyntaxException, SQLException {
-    if (singleInstance == null) {
-      singleInstance = new PostgresDatabase();
-      if (setUpConnection() == null) {
-        System.out.println(ErrorConstants.ERROR_DATABASE_SETUP);
-      }
-    }
-    return singleInstance;
-  }
+//  /**
+//   * Singleton getter for PostgresDatabase and sets up DB connection.
+//   * @return single instance of PostgresDatabase.
+//   */
+//  public static PostgresDatabase getInstance() throws URISyntaxException, SQLException {
+//    if (singleInstance == null) {
+//      singleInstance = new PostgresDatabase();
+//      if (setUpConnection() == null) {
+//        System.out.println(ErrorConstants.ERROR_DATABASE_SETUP);
+//      }
+//    }
+//    return singleInstance;
+//  }
 
   /**
    * Establishes connection to pg DB.
@@ -46,7 +50,7 @@ public final class PostgresDatabase {
    * @throws URISyntaxException when given improper URI
    * @throws SQLException when driver cannot retrieve conn
    */
-  private static Connection setUpConnection() throws URISyntaxException, SQLException {
+  public static Connection setUpConnection() throws URISyntaxException, SQLException {
     // TODO: encode these details elsewhere
     String tempURI = "postgres://roifqtuewetfej:7d514e978ce5f83a75ca408a356cb5e5324a3657960f95a3533aeb93622c5" +
       "906@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d97rt21a1m7m9k\n";
@@ -71,7 +75,7 @@ public final class PostgresDatabase {
    * Inserts a new user into the users table (sign-up).
    * @param data list of user-related data to insert
    */
-  public static void insertUser(List<Object> data) {
+  public static void insertUser(List<Object> data) throws SQLException {
     String insertString = Queries.insertUserQuery();
 
     // Insert into users table
@@ -83,6 +87,7 @@ public final class PostgresDatabase {
       stmt.execute();
     } catch (SQLException ex) {
       System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
     }
 
     // Insert into user_preferences table
@@ -93,6 +98,7 @@ public final class PostgresDatabase {
       stmt.execute();
     } catch (SQLException ex) {
       System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
     }
   }
 
@@ -122,6 +128,7 @@ public final class PostgresDatabase {
         }
       } catch (SQLException ex) {
         System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+        throw new SQLException(ex.getMessage());
       }
       return result;
     }
@@ -157,6 +164,7 @@ public final class PostgresDatabase {
         }
       } catch (SQLException ex) {
         System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+        throw new SQLException(ex.getMessage());
       }
       return workouts;
     }
@@ -185,6 +193,7 @@ public final class PostgresDatabase {
         }
       } catch (SQLException ex) {
         System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+        throw new SQLException(ex.getMessage());
       }
 
       // Retrieve each exercise's details
@@ -207,10 +216,14 @@ public final class PostgresDatabase {
               Exercise temp = null;
               exercises.add(temp);
               }
+            } catch (SQLException ex) {
+                System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+                throw new SQLException(ex.getMessage());
             }
           }
         } catch (SQLException ex) {
           System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+          throw new SQLException(ex.getMessage());
         }
 
       return exercises;
@@ -229,7 +242,7 @@ public final class PostgresDatabase {
    * @param createdAt exercise creation timestamp
    */
   public static void insertUserExercise(String username, String exerciseName, String mediaLink, Integer duration,
-                                           String targetArea, String type, String description, Date createdAt) {
+                                           String targetArea, String type, String description, Date createdAt) throws SQLException {
     String insertString = Queries.insertExercise();
 
     // Insert into exercises table
@@ -245,13 +258,14 @@ public final class PostgresDatabase {
       stmt.execute();
     } catch (SQLException ex) {
       System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
     }
   }
 
   /**
    * Helper method for insertUserWorkout. Retrieves exercise ID.
    */
-  public static Integer getExerciseId(String username, String exerciseName){
+  public static Integer getExerciseId(String username, String exerciseName) throws SQLException {
     String queryString = Queries.getExerciseId();
     Integer exerciseId = 0;
 
@@ -266,6 +280,7 @@ public final class PostgresDatabase {
       }
     } catch (SQLException ex) {
       System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
     }
 
     return exerciseId;
@@ -284,7 +299,7 @@ public final class PostgresDatabase {
    * @param exerciseIds list of exercises in the workout
    */
   public static void insertUserWorkout(Date createdAt, Integer duration, String mediaLink, Integer totalLikes,
-                                       String description, String username, String workoutName, List<Integer> exerciseIds) {
+                                       String description, String username, String workoutName, List<Integer> exerciseIds) throws SQLException {
     String insertString = Queries.insertWorkout();
 
     // Insert into workouts table
@@ -300,6 +315,7 @@ public final class PostgresDatabase {
       stmt.execute();
     } catch (SQLException ex) {
       System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
     }
   }
 
@@ -309,16 +325,69 @@ public final class PostgresDatabase {
   public static void deleteUser() {
   }
 
+  /**
+   * Gets all the users the input user follows.
+   * @param id input user ID.
+   * @return list of user ID's.
+   */
   public static List<Integer> getFollowing(int id) {
+    String getString = Queries.getFollowingQuery();
+
+    // Insert into workouts table
+    try (PreparedStatement stmt = dbConn.prepareStatement(getString)) {
+      stmt.setDate(1, createdAt);
+      stmt.setInt(2, duration);
+      stmt.setString(3, mediaLink);
+      stmt.setInt(4, totalLikes);
+      stmt.setArray(5, (Array) exerciseIds);
+      stmt.setString(6, description);
+      stmt.setString(7, username);
+      stmt.setString(8, workoutName);
+      stmt.execute();
+    } catch (SQLException ex) {
+      System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+      throw new SQLException(ex.getMessage());
+    }
     return null;
   }
 
   /**
-   * Queries database for the user specified by input id and adds it to Map of users in Main.
+   * Queries database for the user specified by input id and encapsulates with a AppUser.
    * @param id user ID
    * @return AppUser object containing info about user with given id
    */
   public static AppUser getUser(int id) {
+    //COPY THE CODE AND MODIFFY
+     /**
+     * Retrieves an existing user's information with username.
+     * @param username username
+     * @return list of user info
+     */
+//    public static List<Object> getUserInfo(String username) throws SQLException {
+//      String queryString = Queries.getUserInfo();
+//      List<Object> result = new ArrayList<>();
+//
+//      try (PreparedStatement stmt = dbConn.prepareStatement(queryString)) {
+//        stmt.setString(1, username);
+//
+//        try (ResultSet res = stmt.executeQuery()) {
+//          while (res.next()) {
+//            String firstName = res.getString("first_name");
+//            String lastName = res.getString("last_name");
+//            String workoutType = res.getString("workout_type");
+//            Integer workoutDuration = res.getInt("workout_duration");
+//
+//            result.add(firstName);
+//            result.add(lastName);
+//            result.add(workoutType);
+//            result.add(workoutDuration);
+//          }
+//        } catch (SQLException ex) {
+//          System.out.println(ErrorConstants.ERROR_QUERY_EXCEPTION);
+//        }
+//        return result;
+//      }
+//    }
     return null;
   }
 
@@ -332,4 +401,21 @@ public final class PostgresDatabase {
     return null;
   }
 
+  /**
+   * Adds recently viewed workout recommended to a user to viewed_workouts table.
+   * @param user user id
+   * @param workout workout id
+   */
+  public static void addRecentlyViewed(int user, int workout) {
+
+  }
+
+  /**
+   * Gets user name associated with a user id.
+   * @param userID user id
+   * @return username
+   */
+  public static String getUserName(int userID) {
+    return null;
+  }
 }
--- src/main/java/edu/brown/cs/everybody/userComponents/UserHandlers.java
+++ src/main/java/edu/brown/cs/everybody/userComponents/UserHandlers.java
@@ -75,95 +75,88 @@ public class UserHandlers {
     }
   }
 
+  /**
+   * Handles requests made for home feed recommendations.
+   */
+  public static class GetRecommendationsHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+      // TODO: filter out workouts which don't match preferences in the for loops
+      JSONObject data = new JSONObject(request.body());
+      // List of Workouts to return to frontend
+      List<Map<String, String>> output = new ArrayList<>();
 
-    /**
-     * Handles requests made for home feed recommendations.
-     */
-    public static class GetRecommendationsHandler implements Route {
-      @Override
-      public Object handle(Request request, Response response) throws Exception {
-        // TODO: filter out workouts which don't match preferences in the for loops
-
-
-        JSONObject data = new JSONObject(request.body());
-        // List of Workouts to return to frontend
-        List<Map<String, String>> output = new ArrayList<>();
-
-        // Parse request from client and extract user info
-        int userID = Integer.parseInt(data.getString("userID"));
-        AppUser user;
-        if (Main.getUsers().containsKey(userID)) {
-          user = Main.getUsers().get(userID);
-        } else {
-          user = PostgresDatabase.getUser(userID);
-          // TODO: instead of checking for null, catch the exception
-          if (user == null) {
-            Map<String, Object> variables = ImmutableMap.of("error", "UserID Not Found.");
-            return GSON.toJson(variables);
-          }
-        }
-
-        // inserts 10 workouts from people user follows into finalSortedWorkouts
-        PriorityQueue<Workout> finalSortedWorkouts = new PriorityQueue<>(new WorkoutComparator());
-        int counter = 0;
-        List<Integer> following = user.getFollowing();
-        Collections.shuffle(following);
-        for (int followingUserID : following) {
-          if (counter == 10) {
-            break;
-          }
-          AppUser followingUser = PostgresDatabase.getUser(followingUserID);
-          // TODO: catch exceptions
-          PriorityQueue<Workout> followingUserWorkouts = new PriorityQueue<>(new WorkoutComparator());
-          if (followingUser.getWorkouts() != null) {
-            followingUserWorkouts.addAll(followingUser.getWorkouts());
-          }
-          Workout recentPost = followingUserWorkouts.poll();
-          while (recentPost != null) {
-            if (!user.getRecentlyViewed().contains(recentPost.getWorkoutId())) {
-              user.addRecentlViewed(recentPost.getWorkoutId());
-              finalSortedWorkouts.add(recentPost);
-              counter++;
-              break;
-            } else {
-              recentPost = followingUserWorkouts.poll();
-            }
-          }
-        }
-
-        // inserts 4 workouts from strongly connected users into output
-        counter = 0;
-        List<Integer> scc = user.getStronglyConnected();
-        Collections.shuffle(scc);
-        for (int connectedUserID : scc) {
-          if (counter == 4) {
-            break;
-          }
-          AppUser connectedUser = PostgresDatabase.getUser(connectedUserID);
-          // TODO: catch exceptions
-          PriorityQueue<Workout> connectedUserWorkouts = new PriorityQueue<>(new WorkoutComparator());
-          if (connectedUser.getWorkouts() != null) {
-            connectedUserWorkouts.addAll(connectedUser.getWorkouts());
-          }
-          Workout recentPost = connectedUserWorkouts.poll();
-          while (recentPost != null) {
-            if (!user.getRecentlyViewed().contains(recentPost.getWorkoutId())) {
-              user.addRecentlViewed(recentPost.getWorkoutId());
-              finalSortedWorkouts.add(recentPost);
-              counter++;
-              break;
-            } else {
-              recentPost = connectedUserWorkouts.poll();
-            }
-          }
-        }
-        Workout finalWorkout = finalSortedWorkouts.poll();
-        while (finalWorkout != null) {
-          output.add(finalWorkout.toMap());
-        }
-        Map<String, Object> variables = ImmutableMap.of("output", output);
+      // Parse request from client and extract user info
+      int userID = Integer.parseInt(data.getString("userID"));
+      AppUser user = PostgresDatabase.getUser(userID);
+      // TODO: instead of checking for null, catch the exception
+      if (user == null) {
+        Map<String, Object> variables = ImmutableMap.of("error", "UserID Not Found.");
         return GSON.toJson(variables);
       }
+
+      // inserts 10 workouts from people user follows into finalSortedWorkouts
+      PriorityQueue<Workout> finalSortedWorkouts = new PriorityQueue<>(new WorkoutComparator());
+      int counter = 0;
+      List<Integer> following = user.getFollowing();
+      Collections.shuffle(following);
+      for (int followingUserID : following) {
+        if (counter == 10) {
+          break;
+        }
+        AppUser followingUser = PostgresDatabase.getUser(followingUserID);
+        // TODO: catch exceptions
+        PriorityQueue<Workout> followingUserWorkouts = new PriorityQueue<>(new WorkoutComparator());
+        if (followingUser.getWorkouts() != null) {
+          followingUserWorkouts.addAll(followingUser.getWorkouts());
+        }
+        Workout recentPost = followingUserWorkouts.poll();
+        while (recentPost != null) {
+          if (!user.getRecentlyViewed().contains(recentPost.getWorkoutId())) {
+            user.addRecentlyViewed(recentPost.getWorkoutId());
+            finalSortedWorkouts.add(recentPost);
+            counter++;
+            break;
+          } else {
+            recentPost = followingUserWorkouts.poll();
+          }
+        }
+      }
+
+      // inserts 4 workouts from strongly connected users into output
+      counter = 0;
+      List<Integer> scc = user.getStronglyConnected();
+      Collections.shuffle(scc);
+      for (int connectedUserID : scc) {
+        if (counter == 4) {
+          break;
+        }
+        AppUser connectedUser = PostgresDatabase.getUser(connectedUserID);
+        // TODO: catch exceptions
+        PriorityQueue<Workout> connectedUserWorkouts = new PriorityQueue<>(new WorkoutComparator());
+        if (connectedUser.getWorkouts() != null) {
+          connectedUserWorkouts.addAll(connectedUser.getWorkouts());
+        }
+        Workout recentPost = connectedUserWorkouts.poll();
+        while (recentPost != null) {
+          if (!user.getRecentlyViewed().contains(recentPost.getWorkoutId())) {
+            user.addRecentlyViewed(recentPost.getWorkoutId());
+            finalSortedWorkouts.add(recentPost);
+            counter++;
+            break;
+          } else {
+            recentPost = connectedUserWorkouts.poll();
+          }
+        }
+      }
+      Workout finalWorkout = finalSortedWorkouts.poll();
+      while (finalWorkout != null) {
+        output.add(finalWorkout.toMap());
+        finalWorkout = finalSortedWorkouts.poll();
+      }
+      Map<String, Object> variables = ImmutableMap.of("output", output);
+      return GSON.toJson(variables);
     }
   }
+}
 
