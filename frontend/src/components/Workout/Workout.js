06a2d97be1b5161880bf69a84a2580cee672fc8e
Lauren Choi <laurenchoi@gmail.com>
2021-04-11 03:10:53 -0400

    redirect workout items to individual workout page

--- frontend/src/components/Workout/Workout.js
+++ frontend/src/components/Workout/Workout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Workout.css';
 
 function Workout(props){
+  const url = "http://localhost:3000/workout/" + props.id
   return(
     <div className="workout-container">
       <img className="workout-image" src={props.thumbnail}/>
@@ -9,7 +10,7 @@ function Workout(props){
         <h5>{props.title}</h5>
         <p className="workout-info">{props.user} | {props.duration}</p>
       </div>
-      <button className="workout-btn">Start workout</button>
+      <a href={url}><button className="workout-btn">Start workout</button></a>
     </div>
   );
 }
