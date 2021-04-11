4aeae2360eb30dbd7c1d40adfa0531ea717fa8af
Lauren Choi <laurenchoi@gmail.com>
2021-04-11 04:49:50 -0400

    pass in fetched exercises to exercise component

--- frontend/src/pages/ExercisePage/ExercisePage.js
+++ frontend/src/pages/ExercisePage/ExercisePage.js
@@ -1,21 +1,25 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import './ExercisePage.css';
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Exercise from "./components/Exercise";
+import { getAllExercises } from "../../api";
 
-function ExercisePage(props){
+function ExercisePage() {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    setExercises(getAllExercises());
+  }, []);
 
   return (
-      <div className="exercise-page">
-          <div className = "sidebar">
-            <Sidebar></Sidebar>
-          </div>
-          <div className = "exercises">
-              <Exercise/>
-
-          </div>
-
+    <div className="exercise-page">
+      <div className = "sidebar">
+        <Sidebar />
       </div>
+      <div className = "exercises">
+        <Exercise exercises={exercises}/>
+      </div>
+    </div>
   );
 }
 export default ExercisePage
