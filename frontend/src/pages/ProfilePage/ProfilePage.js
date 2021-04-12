ed3de42b3ed9cea5edce698cccd196b9e54fc595
Lauren Choi <laurenchoi@gmail.com>
2021-04-12 16:33:23 -0400

    remove logout button from toolbar, pseudocode for following list

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -14,6 +14,13 @@ function ProfilePage(props){
   const sampleData = [{id: 1, title: "Workout 1", duration: "15 min", user: "Tim Nelson", thumbnail: "https://blog.fitbit.com/wp-content/uploads/2018/08/0816-summer-workouts-HERO.jpg", category: "cardio", tags: ["arms", "abs"]}, {id: 2, title: "Workout 2", duration: "30 min", user: "Spike", thumbnail: "https://cdn10.phillymag.com/wp-content/uploads/2016/12/running-shoe-cecilie-arcurs-istock-940x540.jpg", category: "bodyweight", tags: ["legs"]}, 
   {id: 3, title: "Workout 3", duration: "20 min", user: "Andy Van Dam", thumbnail: "https://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Fitness/Articles/Twice+a+Day/man+working+out-carousel.jpg", category: "weights", tags: ["legs", "abs"]}];
 
+
+  const allFollowing = []; // results of getFollowing
+  const displayFollowing = []; // set to allFollowing
+
+  // after unfollow
+  // splice the user from displayFollowing and rerender the component with displayFollowing
+
   useEffect(() => {
     getWorkouts();
     renderWorkouts();
@@ -23,7 +30,6 @@ function ProfilePage(props){
   const renderWorkouts = () => {
     setWorkouts(sampleData.map((exercise) => <Workout key={exercise.id} id={exercise.id} title={exercise.title} duration={exercise.duration} user={exercise.user} thumbnail={exercise.thumbnail}/>))
   }
-
 
   // makes an api request and sets initial state variable
   const getUserWorkouts = async () => {
