50eec3f7bc866c8bc9d039fc1c7e2ff9c9707d93
Lauren Choi <laurenchoi@gmail.com>
2021-04-15 08:38:53 -0400

    update styling for profile workout cards

--- frontend/src/pages/ProfilePage/components/WorkoutSelf/WorkoutSelf.js
+++ frontend/src/pages/ProfilePage/components/WorkoutSelf/WorkoutSelf.js
@@ -1,14 +1,22 @@
 import React from 'react'; 
+import {Card} from 'react-bootstrap'
 import './WorkoutSelf.css';
 
 function WorkoutSelf(props){
+  const url = "/workout/" + props.workout.workout_id
   return (
-    <div className="workout-self">
-      <h4>{props.name}</h4>
-      <p>Duration: {Math.floor(props.duration/60)} minutes</p>
-      <p>Description: {props.description}</p>
-      <p>Created at: {props.createdAt}</p>
-    </div>
+    <Card className="workout-self">
+      <Card.Img variant="top" src="https://runningmagazine.ca/wp-content/uploads/2013/07/164767502.jpg" />
+      <Card.Body>
+        <Card.Text>
+          <div className="workout-title">
+            <h4>{props.workout.workout_name}</h4>
+          </div>
+          <p>Duration: {Math.floor(props.workout.duration/60)} minutes<br></br>Description: {props.workout.description}</p>
+          <a href={url}><button id="start-workout-btn" className="submit-btn" >Start workout</button></a>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 export default WorkoutSelf
