de36de4a3dcf66d7cfd6b821c282f13b44021f90
jwoo153 <49794075+jwoo153@users.noreply.github.com>
2021-04-16 01:37:54 -0400

    deselecting updates exercises automatically as well

--- frontend/src/pages/ExercisePage/ExercisePage.js
+++ frontend/src/pages/ExercisePage/ExercisePage.js
@@ -5,7 +5,10 @@ import Exercise from "./components/Exercise";
 import axios from "axios";
 
 function ExercisePage() {
+    const [allEx, setAllEx]=useState({})
+    const [checked, setChecked] = useState([])//names of checked
     const [exercises, setExercises] = useState({});
+
     const newGetAllExercises = async () => {
       let config = {
         headers: {
@@ -24,6 +27,7 @@ function ExercisePage() {
         console.log(keys)
         console.log(data)
         setExercises(response.data)
+        setAllEx(response.data)
       })
       .catch(function (error) {
         console.log(error);
@@ -39,24 +43,63 @@ function ExercisePage() {
       console.log(exercises)
 
       if (e.target.checked) {
+          /*
+          if checked, it iterates through exercises and adds only ones that contain the tag name to show
+           */
+          checked.push(e.target)
           let filtered = Object.fromEntries(Object.entries(exercises).filter(([k,v]) => v[4].includes(e.target.name)));
           setExercises(filtered)
           console.log(filtered)
+          console.log(checked)
       } else {
+          /*
+          this should only run if it was initially checked and now unchecked
+          Unchecking means that more results should appear
+          1. get a list of all of the leftover checks
+          2. do a for loop where you do the similar thing as in the target checked
+          arr = arr.filter(e => e !== 'B')
+           */
+          let arr = checked.filter(box=> box !=e.target)
+          setChecked(arr)
+          console.log(arr.length)
+          console.log(checked.length)
+          if (arr.length >0) {
+              console.log("here")
+              arr.forEach(id => {
+                  console.log(id)
+                      let filtered = Object.fromEntries(Object.entries(allEx).filter(([k, v]) => v[4].includes(id.name)));
+                      setExercises(filtered)
+                  }
+              )
+          } else {
+              console.log("setting to allEx")
+              setExercises(allEx)
+          }
+          // let filtered = Object.fromEntries(Object.entries(exercises).filter(([k,v]) => v[4].includes(e.target.name)));
+          // setExercises(filtered)
+          //console.log(filtered)
+
           console.log('Not checked');
+          console.log(arr)
       }
 
 
   };
+  const getChecked =()=>{
+
+  }
 
   const resetEx=()=>{
       newGetAllExercises()
+      /*
+      resetting exercises--> deselect the checkboxes
+       */
   };
 
   return (
     <div className="exercise-page">
       <div className = "sidebar">
-        <Sidebar resetEx = {resetEx} updateExercises = {updateExercises} />
+        <Sidebar  resetEx = {resetEx} updateExercises = {updateExercises} />
       </div>
       <div className = "exercises">
         <Exercise  exercises={exercises}/>
--- frontend/src/pages/ExercisePage/components/Sidebar/Sidebar.js
+++ frontend/src/pages/ExercisePage/components/Sidebar/Sidebar.js
@@ -3,16 +3,10 @@ import './Sidebar.css';
 import {Button} from "react-bootstrap";
 
 function Sidebar(props){
-  /*
-  1. may want to have all of the exercises intially rendered/displayed as a json object
-  this way we can organize/display the keys
-   */
 
-    // const [abs, setAbs] = useState(false);
-    // const [chest, setChest] = useState(false);
-    // const [legs, setLegs] = useState(false);
 
     const checkClick = (e)=> {
+        props.resetEx()
         // var {name, checked} = e.target
 
         /*
@@ -51,17 +45,6 @@ function Sidebar(props){
           <li>
           <input type = "checkbox" name = "arms" id = "arms" onChange={props.updateExercises}/> <label for="abs"> Arms</label>
           </li>
-      </ul>
-      <hr/>
-
-        <h3>Ratings</h3>
-      <ul className = "ratings">
-          <li><input type = "checkbox" name = "1 Star" id = "1" onChange={checkClick}/> <label for="1"> 1 Star</label></li>
-          <li><input type = "checkbox" name = "2 Star" id = "2" onChange={checkClick}/> <label for="2"> 2 Star</label></li>
-          <li><input type = "checkbox" name = "3 Star" id = "3" onChange={checkClick}/> <label for="3"> 3 Star</label></li>
-          <li><input type = "checkbox" name = "4 Star" id="4" onChange={checkClick}/> <label for="4"> 4 Star</label></li>
-          <li><input type = "checkbox" name = "5 Star" id = "5" onChange={checkClick}/> <label for="5"> 5 Star</label></li>
-
       </ul>
       <hr/>
 
